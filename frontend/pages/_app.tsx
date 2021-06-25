9ffbe7f4c04fa0c46ca9adb99530d3765a4d3303
Soorria Saruva <soorria.ss@gmail.com>
2021-06-25 22:11:17 +1000

    janky hack to fix dev styling issues no longer needed

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,5 @@
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
-import { NoSsr } from '@material-ui/core'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import { useEffect } from 'react'
 
@@ -22,14 +21,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         />
       </Head>
       <CssBaseline />
-      {/* Janky hack to prevent styling issues in dev */}
-      {process.env.NODE_ENV === 'production' ? (
-        <Component {...pageProps} />
-      ) : (
-        <NoSsr>
-          <Component {...pageProps} />
-        </NoSsr>
-      )}
+      <Component {...pageProps} />
     </>
   )
 }
