67989b5ccf75aef16f467c7d372f63488322294b
josephjeong <joe.b.jeong@gmail.com>
2021-06-26 19:14:22 +1000

    ts-jest running with correct folder structure

--- backend/tests/profiles/profiles-create.test.ts
+++ backend/tests/profiles/profiles-create.test.ts
@@ -1,4 +1,4 @@
-import {sum} from '../src/profiles/profiles-create';
+import {sum} from '../../src/profiles/profiles-create';
 
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
