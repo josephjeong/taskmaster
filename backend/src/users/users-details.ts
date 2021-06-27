51cea975c5c19f0823e7d2a877316114782e8207
josephjeong <joe.b.jeong@gmail.com>
2021-06-27 19:15:43 +1000

    fetchUserDetails function added:

--- backend/src/users/users-details.ts
+++ backend/src/users/users-details.ts
@@ -2,3 +2,25 @@
     file to allow users to see other people's profiles
 */
 
+import { User } from "../entity/User";
+import { getConnection } from "typeorm";
+import { UserDetails } from "./users-interface";
+
+export async function fetchUserDetails(id : string) : Promise<UserDetails> {
+    const user = await getConnection().getRepository(User).find({where : {id : id}});
+    if (user.length != 1) {throw "This Account does not exist";}
+
+    let details : UserDetails =
+     {
+        email: user[0].email,
+        first_name: user[0].first_name,
+        last_name: user[0].last_name,
+        bio: user[0].bio
+    };
+
+    if ('avatar_url' in user[0]) {
+        details.avatar_url = user[0].avatar_url;
+    }
+    
+    return details;
+}
