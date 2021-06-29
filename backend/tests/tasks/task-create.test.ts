aca77fc55e4d308c08320a7fdce76da7b2a5f4ca
Bowen Dong <username@live.com.au>
2021-06-29 19:06:04 +1000

    added editTask and tests, updated createTask and tests

--- backend/tests/tasks/task-create.test.ts
+++ backend/tests/tasks/task-create.test.ts
@@ -20,6 +20,7 @@ test('correct task creation', async () => {
     const task_project: string = null;
     const task_title = "title";
     const task_deadline = new Date();
+    task_deadline.setSeconds(task_deadline.getSeconds() + 1);
     const task_status = Status.NOT_STARTED;
     const task_description = "description\n";
     const task_estimated_days = 2.5;
@@ -38,6 +39,64 @@ test('correct task creation', async () => {
     expect(task[0].status).toBe(task_status);
     expect(task[0].description).toBe(task_description);
     expect(task[0].estimated_days).toBe(task_estimated_days);
+});
+
+test('invalid status test', async () => {
+    expect.assertions(1);
+    await createUser(
+        "asd@gmail.com","badpassword","bob","dob","asd"
+    )
+    const user = await getConnection().getRepository(User).find({where : {email : "asd@gmail.com"}});
+    const task_creator = user[0].id;
+    const task_project: string = null;
+    const task_title = "title";
+    const task_deadline = new Date();
+    const task_status = "bad status";
+    const task_description = "description\n";
+    const task_estimated_days = 2.5;
+    return expect(createTask(
+        task_creator, task_title, task_deadline, 
+        task_status as any, task_project, task_description, task_estimated_days
+    )).rejects.toEqual("invalid task status");
+});
+
+test('invalid deadline test', async () => {
+    expect.assertions(1);
+    await createUser(
+        "asd@gmail.com","badpassword","bob","dob","asd"
+    )
+    const user = await getConnection().getRepository(User).find({where : {email : "asd@gmail.com"}});
+    const task_creator = user[0].id;
+    const task_project: string = null;
+    const task_title = "title";
+    const task_deadline = new Date(); // sets time to now
+    const task_status = Status.NOT_STARTED;
+    const task_description = "description\n";
+    const task_estimated_days = 2.5;
+    return expect(createTask(
+        task_creator, task_title, task_deadline, 
+        task_status, task_project, task_description, task_estimated_days
+    )).rejects.toEqual("deadline must be in the future");
+});
+
+test('invalid estimated_days test', async () => {
+    expect.assertions(1);
+    await createUser(
+        "asd@gmail.com","badpassword","bob","dob","asd"
+    )
+    const user = await getConnection().getRepository(User).find({where : {email : "asd@gmail.com"}});
+    const task_creator = user[0].id;
+    const task_project: string = null;
+    const task_title = "title";
+    const task_deadline = new Date();
+    task_deadline.setSeconds(task_deadline.getSeconds() + 1);
+    const task_status = Status.NOT_STARTED;
+    const task_description = "description\n";
+    const task_estimated_days = 0.000;
+    return expect(createTask(
+        task_creator, task_title, task_deadline, 
+        task_status, task_project, task_description, task_estimated_days
+    )).rejects.toEqual("estimated_days must be >= 0");
 });
 
 beforeAll(async () => {
