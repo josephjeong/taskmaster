263a1c7033a2ecc5f565229898c620915062cd79
Everyhings-Alright <jecylhing@gmail.com>
2021-06-29 16:46:48 +1000

    added connections and connection test

---
+++ backend/tests/connections/connections.test.ts
@@ -0,0 +1,150 @@
+/*
+    test file to test users-update.ts file
+*/
+
+import { createConnection, getConnection } from "typeorm";
+
+import {User} from "../../src/entity/User";
+import { Connection } from "../../src/entity/Connection";
+import { createUser } from "../../src/users/users-create";
+import { createUserConnection, acceptRequest, declineRequest } from "../../src/connection";
+import { clearEntity } from "../test-helpers/clear";
+
+beforeAll(async () => {
+    await createConnection();
+});
+
+beforeEach(async () => {
+    await clearEntity(Connection);
+    await clearEntity(User);
+});
+
+/** clear out database after all tests run */
+afterAll(async () => {
+    await clearEntity(Connection);
+    await clearEntity(User);
+    return await getConnection().close()
+});
+
+// 1. test creation of connection. 
+test('Connection correctly created', async () => {
+    const user_email = 'validemail@webiste.com'
+    const user_password = 'strong password'
+    const user_first_name = 'dude';
+    const user_last_name = 'bro';
+    const user_bio = 'an awesome person';
+    await createUser(
+        user_email,
+        user_password,
+        user_first_name,
+        user_last_name,
+        user_bio
+    )
+
+    const user_email1 = 'validemail888@webiste.com'
+    const user_password1 = 'stronger password'
+    const user_first_name1 = 'dudeman';
+    const user_last_name1 = 'broseph';
+    const user_bio1 = 'an awesome person with a moustache';
+    await createUser(
+        user_email1,
+        user_password1,
+        user_first_name1,
+        user_last_name1,
+        user_bio1
+    )
+
+    const userRepo = getConnection().getRepository(User);
+    const user1 = await userRepo.findOne({where : {email : user_email}});
+    const user2 = await userRepo.findOne({where : {email : user_email1}});
+
+    await createUserConnection(user1.id, user2.id);
+    
+    const connRepo = getConnection().getRepository(Connection);
+    //for whatever reason the requestee and requester ids are switched here during lookup
+    const conn = await connRepo.findOne({where : {requestee : user1.id, requester: user2.id}});
+    console.log(conn);
+    expect(conn.accepted).toEqual(false);
+    expect(conn.requestee).toBe(user1.id);
+    expect(conn.requester).toBe(user2.id);
+    
+});
+// 2. test accepting of connection.
+test('Connection correctly accepted', async () => {
+    const user_email = 'validemail@webiste.com'
+    const user_password = 'strong password'
+    const user_first_name = 'dude';
+    const user_last_name = 'bro';
+    const user_bio = 'an awesome person';
+    await createUser(
+        user_email,
+        user_password,
+        user_first_name,
+        user_last_name,
+        user_bio
+    )
+
+    const user_email1 = 'validemail888@webiste.com'
+    const user_password1 = 'stronger password'
+    const user_first_name1 = 'dudeman';
+    const user_last_name1 = 'broseph';
+    const user_bio1 = 'an awesome person with a moustache';
+    await createUser(
+        user_email1,
+        user_password1,
+        user_first_name1,
+        user_last_name1,
+        user_bio1
+    )
+
+    const userRepo = getConnection().getRepository(User);
+    const user1 = await userRepo.findOne({where : {email : user_email}});
+    const user2 = await userRepo.findOne({where : {email : user_email1}});
+
+    await createUserConnection(user1.id, user2.id);
+    await acceptRequest(user1.id, user2.id);
+    const connRepo = getConnection().getRepository(Connection);
+    const conn = await connRepo.findOne({where : {requestee : user1.id, requester: user2.id}});
+    console.log(conn);
+    expect(conn.accepted).toEqual(true);
+    expect(conn.requestee).toBe(user1.id);
+    expect(conn.requester).toBe(user2.id);
+    
+});
+// 3. test declination of connection.
+test('Connection correctly accepted', async () => {
+    const user_email = 'validemail@webiste.com'
+    const user_password = 'strong password'
+    const user_first_name = 'dude';
+    const user_last_name = 'bro';
+    const user_bio = 'an awesome person';
+    await createUser(
+        user_email,
+        user_password,
+        user_first_name,
+        user_last_name,
+        user_bio
+    )
+
+    const user_email1 = 'validemail888@webiste.com'
+    const user_password1 = 'stronger password'
+    const user_first_name1 = 'dudeman';
+    const user_last_name1 = 'broseph';
+    const user_bio1 = 'an awesome person with a moustache';
+    await createUser(
+        user_email1,
+        user_password1,
+        user_first_name1,
+        user_last_name1,
+        user_bio1
+    )
+
+    const userRepo = getConnection().getRepository(User);
+    const user1 = await userRepo.findOne({where : {email : user_email}});
+    const user2 = await userRepo.findOne({where : {email : user_email1}});
+
+    await createUserConnection(user1.id, user2.id);
+    await declineRequest(user1.id, user2.id);
+    const connRepo = getConnection().getRepository(Connection);
+    const conn = await connRepo.findOne({where : {requestee : user1.id, requester: user2.id}});
+});
