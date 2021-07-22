b96dc1b1ea4e32c4836a8d48b8656e01376cf96b
thesabinelim <thesabinelim@gmail.com>
2021-07-22 16:01:51 +1000

    Link connections frontend to backend

--- frontend/api/connections.ts
+++ frontend/api/connections.ts
@@ -1,6 +1,6 @@
 import { useCallback } from "react";
 import useSWR, { mutate } from "swr";
-import { ConnectionStatus } from "../types";
+import { ConnectionStatus, User } from "../types";
 import { api } from "./utils";
 
 export const useRequestConnection = () => {
@@ -11,6 +11,38 @@ export const useRequestConnection = () => {
   }, []);
 };
 
+export const useDeleteConnection = () => {
+  return useCallback(async (userId: string) => {
+    mutate(`/connection/status/${userId}`, ConnectionStatus.UNCONNECTED, false);
+    await api.post("/connection/delete", { id: userId });
+    mutate(`/connection/status/${userId}`);
+  }, []);
+};
+
+export const useAcceptConnection = () => {
+  return useCallback(async (userId: string) => {
+    mutate(`/connection/status/${userId}`, ConnectionStatus.CONNECTED, false);
+    await api.post('/connection/accept', { id: userId });
+    mutate(`/connection/status/${userId}`);
+  }, []);
+};
+
+export const useDeclineConnection = () => {
+  return useCallback(async (userId: string) => {
+    mutate(`/connection/status/${userId}`, ConnectionStatus.UNCONNECTED, false);
+    await api.post('/connection/decline', { id: userId });
+    mutate(`/connection/status/${userId}`);
+  }, []);
+};
+
 export const useConnectionStatus = (userId: string) => {
   return useSWR<ConnectionStatus>(`/connection/status/${userId}`);
+};
+
+export const useIncomingConnectionRequests = () => {
+  return useSWR<User[]>(`/connection/incomingRequests`);
+};
+
+export const useOutgoingConnectionRequests = () => {
+  return useSWR<User[]>(`/connection/outgoingRequests`);
 };
--- frontend/components/connections/ConnectionsRequestsModal.tsx
+++ frontend/components/connections/ConnectionsRequestsModal.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { makeStyles, Dialog, DialogTitle, DialogContent, DialogActions, TextField, Button, Avatar, Typography } from '@material-ui/core';
+import { makeStyles, Dialog, DialogTitle, DialogContent, DialogActions, Button, Avatar, Typography } from '@material-ui/core';
 
-import { User } from '../../types';
+import { useAcceptConnection, useDeclineConnection, useDeleteConnection, useIncomingConnectionRequests, useOutgoingConnectionRequests } from '../../api';
 
 const useStyles = makeStyles((theme) => ({
   row: {
@@ -52,108 +52,95 @@ const useStyles = makeStyles((theme) => ({
 
 type ConnectionsModalProps = {
   open: boolean,
-  incoming: User[],
-  setIncoming: (requests: User[]) => void,
-  outgoing: User[],
-  setOutgoing: (requests: User[]) => void,
   onClose: () => void
 };
 
 const ConnectionsModal = ({
   open,
-  incoming,
-  setIncoming,
-  outgoing,
-  setOutgoing,
   onClose
 }: ConnectionsModalProps) => {
-  const [search, setSearch] = React.useState('');
+  const { data: incomingRequests } = useIncomingConnectionRequests();
+  const { data: outgoingRequests } = useOutgoingConnectionRequests();
+
+  const acceptConnection = useAcceptConnection();
+  const declineConnection = useDeclineConnection();
+  const deleteConnection = useDeleteConnection();
 
   const classes = useStyles();
+
+  if (!incomingRequests || !outgoingRequests) {
+    return null;
+  }
 
   return (
     <Dialog
       open={open}
       onClose={() => onClose()}
     >
-      <DialogTitle>Connection Requests</DialogTitle>
-      <form onSubmit={(event) => {}}>
-        <DialogContent>
-          <div className={classes.row}>
-            <TextField
-              label='Search users...'
-              value={search}
-              onChange={(event) => setSearch(event.target.value)}
-            />
-            <Button
-              className={classes.searchButton}
-              size='large'
-              color='primary'
-              variant='contained'
-            >
-              Connect
-            </Button>
+    <DialogTitle>Connection Requests</DialogTitle>
+      <DialogContent>
+        {incomingRequests.length + outgoingRequests.length === 0 ? (
+          <Typography>No requests</Typography>
+        ) : null}
+        {incomingRequests.map((request) => (
+          <div key={request.id} className={classes.user}>
+            <div className={classes.userProfileShort}>
+              <Avatar
+                alt={request.email}
+                src={request.avatar_url}
+                className={classes.avatar}
+              />
+              <Typography>{`${request.first_name} ${request.last_name}`}</Typography>
+            </div>
+            <div className={classes.userActions}>
+              <Button
+                size='medium'
+                color='primary'
+                variant='contained'
+                onClick={() => acceptConnection(request.id)}
+              >
+                Accept
+              </Button>
+              <Button
+                size='medium'
+                onClick={() => declineConnection(request.id)}
+              >
+                Reject
+              </Button>
+            </div>
           </div>
-          {incoming.map((request) => (
-            <div key={request.id} className={classes.user}>
-              <div className={classes.userProfileShort}>
-                <Avatar
-                  alt={request.email}
-                  src={request.avatar_url}
-                  className={classes.avatar}
-                />
-                <Typography>{`${request.first_name} ${request.last_name}`}</Typography>
-              </div>
-              <div className={classes.userActions}>
-                <Button
-                  size='medium'
-                  color='primary'
-                  variant='contained'
-                  onClick={() => {}}
-                >
-                  Accept
-                </Button>
-                <Button
-                  size='medium'
-                  onClick={() => {}}
-                >
-                  Reject
-                </Button>
-              </div>
+        ))}
+        {outgoingRequests.map((request) => (
+          <div key={request.id} className={classes.user}>
+            <div className={classes.userProfileShort}>
+              <Avatar
+                alt={request.email}
+                src={request.avatar_url}
+                className={classes.avatar}
+              />
+              <Typography>{`${request.first_name} ${request.last_name}`}</Typography>
             </div>
-          ))}
-          {outgoing.map((request) => (
-            <div key={request.id} className={classes.user}>
-              <div className={classes.userProfileShort}>
-                <Avatar
-                  alt={request.email}
-                  src={request.avatar_url}
-                  className={classes.avatar}
-                />
-                <Typography>{`${request.first_name} ${request.last_name}`}</Typography>
-              </div>
-              <div className={classes.userActions}>
-                <Button
-                  size='medium'
-                  onClick={() => {}}
-                >
-                  Cancel Request
-                </Button>
-              </div>
+            <div className={classes.userActions}>
+              <Button
+                size='medium'
+                onClick={() => deleteConnection(request.id)}
+              >
+                Cancel Request
+              </Button>
             </div>
-          ))}
-        </DialogContent>
-        <DialogActions>
-          <Button
-            size='large'
-            color='primary'
-            variant='contained'
-            onClick={() => onClose()}
-          >
-            Close
-          </Button>
-        </DialogActions>
-      </form>
+          </div>
+        ))}
+      </DialogContent>
+      <DialogActions>
+        <Button
+          size='large'
+          color='primary'
+          variant='contained'
+          onClick={() => onClose()}
+        >
+          Close
+        </Button>
+      </DialogActions>
     </Dialog>
   )
 };
